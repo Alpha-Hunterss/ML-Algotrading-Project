ab35963686b5263b8aa5d7e165d01b37d6287d02
AmirhsFar <amirhosseinfarahani75@yahoo.com>
2024-11-26 15:20:49 +0330

    leg feature test

--- dataset/configs/feature_configs_general.py
+++ dataset/configs/feature_configs_general.py
@@ -16,7 +16,7 @@ general_config = {
 
     'fe_leg': {
         'timeframe': [5],
-        'window_size': [800, 1400, 2000],
+        'window_size': [800, 1100, 1400],
         'base_columns': ['CLOSE']
     },
 

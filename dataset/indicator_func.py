4237321cb2c3091d842bc41793082f33f3b3e178
AmirhsFar <amirhosseinfarahani75@yahoo.com>
2025-01-26 17:57:17 +0330

    fixed a bug

--- dataset/indicator_func.py
+++ dataset/indicator_func.py
@@ -1317,10 +1317,12 @@ def cal_supertrend_func(
 
     # Calculate basic upper and lower bands
     df = df.with_columns([
-        pl.col(features[1]).cast(pl.Float64),
-        pl.col(features[2]).cast(pl.Float64),
-        ((pl.col(features[1]) + pl.col(features[2])) / 2 + (multiplier * pl.col("atr"))).alias("upper_band"),
-        ((pl.col(features[1]) + pl.col(features[2])) / 2 - (multiplier * pl.col("atr"))).alias("lower_band"),
+        (
+            (pl.col(features[1]) + pl.col(features[2])) / 2 + (pl.lit(multiplier) * pl.col("atr"))
+        ).alias("upper_band"),
+        (
+            (pl.col(features[1]) + pl.col(features[2])) / 2 - (pl.lit(multiplier) * pl.col("atr"))
+        ).alias("lower_band"),
     ])
 
     # Initialize Supertrend columns

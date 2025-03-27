813b7fa9399f96d53a21002b4127c70d3a5fea5c
Arash Shirmohammadi <arash.shi81@gmail.com>
2025-03-27 19:31:43 +0330

    Update window_agg_features_func.py

--- dataset/window_agg_features_func.py
+++ dataset/window_agg_features_func.py
@@ -1,13 +1,21 @@
 from pathlib import Path
 from dataset.configs.history_data_crawlers_config import root_path, symbols_dict
-import pandas as pd
+import polars as pl
 import numpy as np
+import numba
 from dataset.logging_tools import default_logger
 
+def hurst_exponent(ts, max_lag=100):
+    """Calculate the Hurst exponent of a time series using a log-log regression method."""
+    lags = np.arange(2, max_lag)
+    tau = np.array([np.std(np.subtract(ts[lag:], ts[:-lag])) for lag in lags])
+    return np.polyfit(np.log(lags), np.log(tau), 1)[0]
+
+@numba.jit(nopython=True, parallel=True)
 def cal_window_stats(array, window_size):
-    res = np.zeros([array.shape[0], 11])
+    res = np.empty((array.shape[0], 12))  # Increase to store the Hurst exponent
     res[:window_size, :] = np.nan
-    for i in range(window_size, array.shape[0]):
+    for i in numba.prange(window_size, array.shape[0]):
         selected_slice = array[i - window_size + 1 : i + 1]
         res[i, :] = [
             np.min(selected_slice),
@@ -16,11 +24,12 @@ def cal_window_stats(array, window_size):
             1 - (np.argmax(selected_slice) / (window_size - 1)),
             np.mean(selected_slice),
             np.std(selected_slice),
-            pd.Series(selected_slice).skew(),
-            pd.Series(selected_slice).kurtosis(),
+            pl.Series(selected_slice).skew(),
+            pl.Series(selected_slice).kurtosis(),
             np.median(selected_slice),
             np.percentile(selected_slice, 25),
-            np.percentile(selected_slice, 75)
+            np.percentile(selected_slice, 75),
+            hurst_exponent(selected_slice)  # Compute Hurst exponent
         ]
     return res
 
@@ -30,33 +39,27 @@ def add_win_fe_base_func(
     for tf in timeframes:
         for w_size in window_sizes:
             assert tf == 5, "!!! for now, this code only works with 5M timeframe, tf must be 5."
-
-            col_min = f"{fe_prefix}_min_W{w_size}_M{tf}"
-            col_argmin = f"{fe_prefix}_argmin_W{w_size}_M{tf}"
-            col_max = f"{fe_prefix}_max_W{w_size}_M{tf}"
-            col_argmax = f"{fe_prefix}_argmax_W{w_size}_M{tf}"
-            col_mean = f"{fe_prefix}_mean_W{w_size}_M{tf}"
-            col_std = f"{fe_prefix}_std_W{w_size}_M{tf}"
-            col_skew = f"{fe_prefix}_skew_W{w_size}_M{tf}"
-            col_kurt = f"{fe_prefix}_kurt_W{w_size}_M{tf}"
-            col_median = f"{fe_prefix}_median_W{w_size}_M{tf}"
-            col_q25 = f"{fe_prefix}_q25_W{w_size}_M{tf}"
-            col_q75 = f"{fe_prefix}_q75_W{w_size}_M{tf}"
-
+            
+            col_names = [
+                f"{fe_prefix}_min_W{w_size}_M{tf}",
+                f"{fe_prefix}_argmin_W{w_size}_M{tf}",
+                f"{fe_prefix}_max_W{w_size}_M{tf}",
+                f"{fe_prefix}_argmax_W{w_size}_M{tf}",
+                f"{fe_prefix}_mean_W{w_size}_M{tf}",
+                f"{fe_prefix}_std_W{w_size}_M{tf}",
+                f"{fe_prefix}_skew_W{w_size}_M{tf}",
+                f"{fe_prefix}_kurt_W{w_size}_M{tf}",
+                f"{fe_prefix}_median_W{w_size}_M{tf}",
+                f"{fe_prefix}_q25_W{w_size}_M{tf}",
+                f"{fe_prefix}_q75_W{w_size}_M{tf}",
+                f"{fe_prefix}_hurst_W{w_size}_M{tf}"
+            ]
+            
             array = df[raw_features].to_numpy()
             res = cal_window_stats(array, w_size)
-            
-            df[col_min] = (df["M5_CLOSE"] - res[:, 0]) / symbols_dict[symbol]["pip_size"]
-            df[col_argmin] = np.round(res[:, 1], round_to)
-            df[col_max] = (res[:, 2] - df["M5_CLOSE"]) / symbols_dict[symbol]["pip_size"]
-            df[col_argmax] = np.round(res[:, 3], round_to)
-            df[col_mean] = np.round(res[:, 4], round_to)
-            df[col_std] = np.round(res[:, 5], round_to)
-            df[col_skew] = np.round(res[:, 6], round_to)
-            df[col_kurt] = np.round(res[:, 7], round_to)
-            df[col_median] = np.round(res[:, 8], round_to)
-            df[col_q25] = np.round(res[:, 9], round_to)
-            df[col_q75] = np.round(res[:, 10], round_to)
+            df = df.with_columns(
+                [pl.Series(col, res[:, i]).round(round_to) for i, col in enumerate(col_names)]
+            )
     return df
 
 def history_fe_WIN_features(feature_config, logger=default_logger):
@@ -77,13 +80,13 @@ def history_fe_WIN_features(feature_config, logger=default_logger):
             raw_features = [f"M5_{base_col}" for base_col in base_cols]
             needed_columns = ["_time", "minutesPassed", "symbol"] + raw_features
             file_name = base_candle_folder_path + f"{symbol}_realtime_candle.parquet"
-            df = pd.read_parquet(file_name, columns=needed_columns)
-            df.sort_values("_time", inplace=True)
-
-            df["_time"] = df["_time"].dt.tz_localize(None)
-            df.drop(columns=["symbol"], inplace=True)
-            df.sort_values("_time", inplace=True)
-
+            df = pl.read_parquet(file_name).select(needed_columns).sort("_time")
+            
+            df = df.with_columns(
+                df["_time"].cast(pl.Datetime).alias("_time"),
+                df["symbol"].cast(pl.Utf8).alias("symbol")
+            ).drop("symbol")
+            
             df = add_win_fe_base_func(
                 df,
                 symbol,
@@ -93,10 +96,9 @@ def history_fe_WIN_features(feature_config, logger=default_logger):
                 round_to=round_to,
                 fe_prefix="fe_WIN",
             )
-
-            df.drop(columns=raw_features + ["minutesPassed"], inplace=True)
-            df["symbol"] = symbol
-            df.to_parquet(f"{features_folder_path}/{fe_prefix}_{symbol}.parquet")
+            
+            df = df.drop(raw_features + ["minutesPassed"]).with_columns(pl.lit(symbol).alias("symbol"))
+            df.write_parquet(f"{features_folder_path}/{fe_prefix}_{symbol}.parquet")
         logger.info("--> history_fe_WIN_features run successfully.")
     except Exception as e:
         logger.exception("--> history_fe_WIN_features error.")
